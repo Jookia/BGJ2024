2e27a75c167095a7b7e74f4d1f8e1b52223cc0b2
Jookia <contact@jookia.org>
2024-02-10 23:58:11 +1100

    Initial Next.js port

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import './globals.css';
+
+export default function Page() {
+  return <html lang="en">
+      <body>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </body>
+    </html>;
+}
