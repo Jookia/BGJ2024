827808613ea2d84ccaf8d116e2f93a048e069bf9
Jookia <contact@jookia.org>
2024-02-14 01:26:45 +1100

    Dump ARIA code, use anchors

--- src/components/GameOutput.jsx
+++ src/components/GameOutput.jsx
@@ -1,6 +1,6 @@
 import ActionButton from "./ActionButton";
 import parse from "@/helpers/parseText";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 
 export default function GameOutput({
   player,
@@ -8,37 +8,27 @@ export default function GameOutput({
   location,
   setLocation,
 }) {
-  const descriptionRef = useRef();
   const [actionFeedback, setActionFeedback] = useState();
   const p = (text) => parse(text, player);
   const f = (selector) => document.querySelector(selector);
 
-  useEffect(() => {
-    f("#game-output").ariaBusy = false;
-  }, []);
-
   function doAction(action) {
-    f("#game-output").ariaBusy = true;
-    f("#game-output").ariaLive = "polite";
-    if (action.successChance >= Math.random()) {
+    if (!action) {
+      setActionFeedback(null);
+    } else if (action.successChance >= Math.random()) {
       setActionFeedback(action.successText);
       setLocation(action.successTeleport);
     } else {
       // TODO implement failure event
     }
-    f("#game-output").ariaBusy = false;
+    const link = document.createElement('a');
+    link.href = "#location-description";
+    link.click();
   }
+
   return (
-    <div
-      id="game-output"
-      className="p-8 flex flex-col items-center"
-      role="log"
-      aria-atomic="false"
-      aria-busy="true"
-      aria-live="off"
-    >
+    <div id="game-output" className="p-8 flex flex-col items-center">
       <p
-        ref={descriptionRef}
         id="location-description"
         className="mb-4 text-lg"
       >
@@ -49,19 +39,21 @@ export default function GameOutput({
           <>
             <ActionButton
               action={{ text: "Continue" }}
-              doAction={() => setActionFeedback(null)}
+              doAction={() => doAction(null)}
             />
           </>
         ) : (
           <>
-            {Object.entries(location.interactables).map(([name, action]) => (
-              <ActionButton
-                key={name}
-                name={name}
-                action={action}
-                doAction={doAction}
-              />
-            ))}
+            {Object.entries(location.interactables).map(
+              ([name, action], index) => (
+                <ActionButton
+                  key={name}
+                  name={name}
+                  action={action}
+                  doAction={doAction}
+                />
+              ),
+            )}
           </>
         )}
       </div>
