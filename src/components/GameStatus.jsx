8d2d48134667c9654d522078a30fa8132883f86e
Jookia <contact@jookia.org>
2024-02-17 00:24:34 +1100

    Use a phantom div for game focus, use ARIA role statu

--- src/components/GameStatus.jsx
+++ src/components/GameStatus.jsx
@@ -3,9 +3,14 @@ import clsx from "clsx";
 export default function GameStatus({ actionFeedback, locationDescription }) {
   return (
     <>
-      <p aria-live="alert" className="mb-4 text-lg">
+      <p role="status" className="mb-4 text-lg">
         {actionFeedback ?? locationDescription}
       </p>
+      <div
+        id="game-focus"
+        tabIndex="-1"
+        className="focus-visible:outline-none"
+      ></div>
       {actionFeedback && <p className="mb-4 text-lg">{locationDescription}</p>}
     </>
   );
